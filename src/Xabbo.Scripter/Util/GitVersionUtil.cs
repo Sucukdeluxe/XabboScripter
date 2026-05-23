27c6c7fe6f3618399ab3fea8efcf026827aac5b9
QDave <davidav08@gmail.com>
2026-05-23 21:34:00 +0200

    fix version display

--- src/Xabbo.Scripter/Util/GitVersionUtil.cs
+++ src/Xabbo.Scripter/Util/GitVersionUtil.cs
@@ -7,9 +7,18 @@ internal static class GitVersionUtil
 {
     public static string? GetSemanticVersion(Assembly assembly)
     {
-        return assembly
+        var fromGitVersion = assembly
             .GetType("GitVersionInformation")
             ?.GetField("SemVer")
             ?.GetValue(null) as string;
+        if (!string.IsNullOrEmpty(fromGitVersion))
+            return fromGitVersion;
+
+        var info = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        if (string.IsNullOrEmpty(info))
+            return null;
+
+        int plus = info.IndexOf('+');
+        return plus > 0 ? info.Substring(0, plus) : info;
     }
 }
